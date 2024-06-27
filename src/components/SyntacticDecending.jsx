8c8620cfdc8571882a04a470a7e9f15963f27e5c
RobertoIHH <rhernandez49360-5@ccpm.edu.mx>
2024-06-27 03:33:38 -0600

    Terminado partes de conjuntos, reglas y tabla

--- src/components/SyntacticDecending.jsx
+++ src/components/SyntacticDecending.jsx
@@ -1,6 +1,5 @@
 // Definición de la gramática
-const grammar = `
-E  ->  T E'
+const grammar = `E  ->  T E'
 E' ->  + T E' | ε
 T  ->  F T'
 T' ->  * F T' | ε
@@ -8,8 +7,7 @@ F  ->  ( E ) | id
 `;
 
 // Conjuntos Primero calculados manualmente
-const first = `
-P(E)  =  [ ( , id ]
+const first = `P(E)  =  [ ( , id ]
 P(E') =  [ + , ε ]
 P(T)  =  [ ( , id ]
 P(T') =  [ * , ε ]
@@ -17,8 +15,7 @@ P(F)  =  [ ( , id ]
 `;
 
 // Conjuntos Siguiente calculados manualmente
-const follow = `
-S(E)  =  [ ) , $ ]
+const follow = `S(E)  =  [ ) , $ ]
 S(E') =  [ ) , $ ]
 S(T)  =  [ + , ) , $ ]
 S(T') =  [ + , ) , $ ]
@@ -78,18 +75,22 @@ export default function SyntacticDecending() {
         <h2 className="text-xl font-semibold mb-2">
           Reglas para la creación de la Tabla LL(1)
         </h2>
-        <div className="bg-gray-100 p-4 rounded-md mb-4">
+        <div className="bg-gray-100 p-2 rounded-md mb-4 text-justify">
           <ul className="list-disc list-inside">
             <li>
               Para cada producción A → α:
               <ul className="list-disc list-inside ml-4">
                 <li>
-                  Para cada terminal a en el Conjunto Primero(α), añade A → α a
-                  la entrada [A, a] en la tabla.
+                  Para cada terminal 'a' en el Conjunto Primero(α), añade la producción A → α a la entrada [A, a] en la tabla.
                 </li>
                 <li>
-                  Si ε está en el Conjunto Primero(α), para cada terminal b en
-                  el Conjunto Siguiente(A), añade A → α a la entrada [A, b] en
+                  Si ε está en el Conjunto Primero(α), para cada terminal 'b' en
+                  el Conjunto Siguiente(A), añade la producción A → α a la entrada [A, b] en
+                  la tabla.
+                </li>
+                <li>
+                  Si el simbolo de entrada $ está en el Siguiente(A), 
+                  añade la producción A → α a la entrada [A, $] en
                   la tabla.
                 </li>
               </ul>
@@ -103,42 +104,50 @@ export default function SyntacticDecending() {
           Ejemplo con la Gramática Dada
         </h2>
         <div className="bg-gray-100 p-4 rounded-md mb-4">
-          <pre className="whitespace-pre-wrap">
-            {`1. Para la producción E → T E':
-   - Conjunto Primero(T E') = Conjunto Primero(T) = { (, id }
-   - Añadir "E → T E'" en las entradas [E, (] y [E, id] de la tabla.
-
-2. Para la producción E' → + T E':
-   - Conjunto Primero(+ T E') = { + }
-   - Añadir "E' → + T E'" en la entrada [E', +] de la tabla.
-
-3. Para la producción E' → ε:
-   - ε está en el Conjunto Primero(E')
-   - Conjunto Siguiente(E') = { ), $ }
-   - Añadir "E' → ε" en las entradas [E', )] y [E', $] de la tabla.
-
-4. Para la producción T → F T':
-   - Conjunto Primero(F T') = Conjunto Primero(F) = { (, id }
-   - Añadir "T → F T'" en las entradas [T, (] y [T, id] de la tabla.
-
-5. Para la producción T' → * F T':
-   - Conjunto Primero(* F T') = { * }
-   - Añadir "T' → * F T'" en la entrada [T', *] de la tabla.
-
-6. Para la producción T' → ε:
-   - ε está en el Conjunto Primero(T')
-   - Conjunto Siguiente(T') = { +, ), $ }
-   - Añadir "T' → ε" en las entradas [T', +], [T', )] y [T', $] de la tabla.
-
-7. Para la producción F → ( E ):
-   - Conjunto Primero(( E )) = { ( }
-   - Añadir "F → ( E )" en la entrada [F, (] de la tabla.
-
-8. Para la producción F → id:
-   - Conjunto Primero(id) = { id }
-   - Añadir "F → id" en la entrada [F, id] de la tabla.
-`}
-          </pre>
+          <div className="bg-gray-900 text-white p-4 mt-4 rounded-md">
+            <ol className="list-decimal list-inside font-bold">
+              <li><strong className=" font-normal">Para la producción E → T E':</strong></li>{/* Tabla E */}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(T E') = Conjunto Primero(T) = {"{ (, id }"}</li>
+                <li>Añadir "E → T E'" en las entradas de la tabla { "[ E, ( ] y [ E, id ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción E' → + T E':</strong></li>{/* Tabla E' */}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(+ T E') = {"{ + }"}</li>
+                <li>Añadir "E' → + T E'" en la entrada de la tabla { "[ E', + ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción E' → ε:</strong></li>{/* Tabla  E' en vacio*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Siguiente(E') = {"{ ), $ }"}</li>
+                <li>Añadir "E' → ε" en las entradas de la tabla { "[ E', ) ] y [ E', $ ] "}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción T → F T':</strong></li>{/* Tabla  T*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(F T') = Conjunto Primero(F) = {"{ (, id }"}</li>
+                <li>Añadir "T → F T'" en las entradas de la tabla { "[ T, ( ] y [ T, id ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción T' → * F T':</strong></li>{/* Tabla  T'*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(* F T') = {"{ * }"}</li>
+                <li>Añadir "T' → * F T'" en la entrada de la tabla { "[ T', * ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción T' → ε:</strong></li>{/* Tabla  T' en vacio*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Siguiente(T') = {"{ +, ), $ }"}</li>
+                <li>Añadir "T' → ε" en las entradas de la tabla { "[ T', + ], [ T', ) ] y [ T', $ ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción F → ( E ):</strong></li>{/* Tabla  F*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(( E )) = {"{ ( }"}</li>
+                <li>Añadir "F → ( E )" en la entrada de la tabla { "[ F, ( ]"}</li>
+              </ul>
+              <li><strong className=" font-normal">Para la producción F → id:</strong></li>{/* Tabla  F en id*/}
+              <ul className="list-disc list-inside ml-6 font-normal">
+                <li>Conjunto Primero(id) ={"{ id }"}</li>
+                <li>Añadir "F → id" en la entrada de la tabla { "[ F, id ]"}</li>
+              </ul>
+            </ol>
+          </div>
         </div>
       </div>
 
@@ -173,9 +182,10 @@ export default function SyntacticDecending() {
 
       <div className="mt-8">
         <h2 className="text-xl font-semibold mb-2">Explicación Adicional</h2>
-        <div className="bg-gray-100 p-4 rounded-md mb-4">
-          <h3 className="text-lg font-semibold mb-2">
+        <div className="bg-gray-100 p-4 rounded-md mb-4">{/* ------------------Seccion de reglas de conjuntos */}
+          <h3 className="text-xl font-black font-serif mb-2">
             Reglas para calcular el Conjunto Primero:
+            {/* Reglas del conjunto primero */}
           </h3>
           <ul className="list-disc list-inside">
             <li>Si existe una producción A → α B β | ε :</li>
@@ -229,10 +239,12 @@ export default function SyntacticDecending() {
               </ul>
             </li>
           </ul>
-          <h3 className="text-lg font-semibold mt-4 mb-2">
+          <h3 className="text-xl font-black font-serif mb-2">
             Reglas para calcular el Conjunto Siguiente:
+            {/* Reglas del conjunto siguiente */}
           </h3>
           <ul className="list-disc list-inside">
+            <li>Si existe una producción A → α B β:</li>
             <li>
               El Conjunto Siguiente(A) incluye `$` (donde A es el símbolo
               inicial), el $ solo se coloca en el símbolo inicial
@@ -240,34 +252,41 @@ export default function SyntacticDecending() {
             <li>
               El conjunto Siguiente de B es igual al conjunto primero de β
             </li>
-            <li>
-              Si existe una producción A → α B β:
-              <ul className="list-disc list-inside ml-4">
-                <li className="  text-red-500">
-                  Añadir el Conjunto Primero(β) - {`{ε}`} al Conjunto
-                  Siguiente(B) posible
-                </li>
-                <li className="  text-red-500">
-                  Si β deriva a ε (o β es ε), añadir el Conjunto Siguiente(A) al
-                  Conjunto Siguiente(B) posible
-                </li>
-              </ul>
-            </li>
+            <ul className="list-disc list-inside ml-4">
+              <li className="  text-red-500">
+                Añadir el Conjunto Primero(β) - {`{ε}`} al Conjunto Siguiente(B)
+                posible
+              </li>
+              <li className="  text-red-500">
+                Si β deriva a ε (o β es ε), añadir el Conjunto Siguiente(A) al
+                Conjunto Siguiente(B) posible
+              </li>
+            </ul>
             <li>
               Si existe una producción T → α A b:
               <p />
               <li>Si existe una producción A → α B:</li>
               <ul className="list-disc list-inside ml-4">
                 <li>
-                  El conjunto Siguiente de B es igual al conjunto siguiente de A
+                  El conjunto Siguiente de B es igual al conjunto siguiente de A debido a que B es el 
+                  ultimo elemento de la producción
+                </li>
+                <li>
+                  Si la producción puede generar ε se debe de considerar, si al ser considerado el conjunto siguiente de B
+                  aun cuenta con un elemento siguiente, se agregaria el conjunto primero de este elemento, si no hay ningun elemento
+                  se tomaria el conjunto siguiente de quien lo produjo a B
                 </li>
               </ul>
             </li>
           </ul>
-          <h3 className="text-lg font-semibold mt-4 mb-2">
-            Cálculo del Conjunto Primero:{/* primero*/}
+        </div>
+        {/* ------------------------------- Cambia el color y la seccion donde se explican los conjuntos */}
+        <div className="bg-gray-900 p-4 rounded-md mb-4">
+          <h3 className="text-2xl font-extrabold mt-4 mb-2 font-serif bg-green-300 ">
+            Cálculo del Conjunto Primero:{/* Conjunto Primero primero*/}
           </h3>
-          <pre className="bg-white p-2 mt-4 rounded-md whitespace-pre-wrap font-bold">{/* perte de la E */}
+          <pre className="bg-white p-2 mt-4 rounded-md whitespace-pre-wrap font-bold">
+            {/* perte de la E */}
             {`P(E) = P(T)
 P(E) = { (, id }`}
           </pre>
@@ -275,8 +294,10 @@ P(E) = { (, id }`}
             <h3 className=" mb-2">Devido a que</h3>
             <li>E → T E'</li>
             <li>
-              Dada esta produccion se tiene que el primer elemento de la produccion de E es <strong className=" text-lg">T</strong> por
-              lo cual se coloca en el P(E) el <strong className=" text-lg"> P(T)</strong>
+              Dada esta produccion se tiene que el primer elemento de la
+              produccion de E es <strong className=" text-lg">T</strong> por lo
+              cual se coloca en el P(E) el{" "}
+              <strong className=" text-lg"> P(T)</strong>
             </li>
           </ul>
           <pre className="bg-white p-2 mt-4 rounded-md whitespace-pre-wrap font-bold">
@@ -288,7 +309,9 @@ P(T) = { (, id)}`}
             <h3 className=" mb-2">Devido a que</h3>
             <li>T → F T'</li>
             <li>
-            Dada esta produccion se tiene que El primer elemento de la produccion de T es <strong className=" text-lg">F</strong> por lo cual se coloca en el P(T) el 
+              Dada esta produccion se tiene que El primer elemento de la
+              produccion de T es <strong className=" text-lg">F</strong> por lo
+              cual se coloca en el P(T) el
               <strong className=" text-lg"> P(F)</strong>
             </li>
           </ul>
@@ -300,11 +323,16 @@ P(F) = { (, id }`}
           <ul className="list-disc list-inside mt-4 mb-2 bg-gray-900 p-2 rounded-md whitespace-pre-wrap text-white">
             <h3 className=" mb-2">Devido a que</h3>
             <li> F → ( E ) | id</li>
-            <li>Dada esta produccion se tiene que el primer elemento de la produccion 
-              de F es <strong className=" text-lg">(</strong> por lo cual se coloca en el P(T) el <strong className=" text-lg">(</strong> </li>
             <li>
-            Tambien se tiene que en esta produccion el primer elemento 
-            puede ser <strong className=" text-lg">id</strong> por lo cual se coloca en el P(T) el 
+              Dada esta produccion se tiene que el primer elemento de la
+              produccion de F es <strong className=" text-lg">(</strong> por lo
+              cual se coloca en el P(T) el{" "}
+              <strong className=" text-lg">(</strong>{" "}
+            </li>
+            <li>
+              Tambien se tiene que en esta produccion el primer elemento puede
+              ser <strong className=" text-lg">id</strong> por lo cual se coloca
+              en el P(T) el
               <strong className=" text-lg"> id</strong>
             </li>
           </ul>
@@ -316,11 +344,16 @@ P(E') = { +, ε }`}
           <ul className="list-disc list-inside mt-4 mb-2 bg-gray-900 p-2 rounded-md whitespace-pre-wrap text-white">
             <h3 className=" mb-2">Devido a que</h3>
             <li> E' → + T E' | ε</li>
-            <li>Dada esta produccion se tiene que el primer elemento de la produccion 
-              de E' es <strong className=" text-xl">+</strong> por lo cual se coloca en el P(E') el <strong className=" text-xl">+</strong> </li>
             <li>
-            Tambien se tiene que en esta produccion el primer elemento 
-            puede ser <strong className=" text-lg">ε</strong> por lo cual se coloca en el P(E') el 
+              Dada esta produccion se tiene que el primer elemento de la
+              produccion de E' es <strong className=" text-xl">+</strong> por lo
+              cual se coloca en el P(E') el{" "}
+              <strong className=" text-xl">+</strong>{" "}
+            </li>
+            <li>
+              Tambien se tiene que en esta produccion el primer elemento puede
+              ser <strong className=" text-lg">ε</strong> por lo cual se coloca
+              en el P(E') el
               <strong className=" text-lg"> ε</strong>
             </li>
           </ul>
@@ -332,16 +365,22 @@ P(T') = { *, ε }`}
           <ul className="list-disc list-inside mt-4 mb-2 bg-gray-900 p-2 rounded-md whitespace-pre-wrap text-white">
             <h3 className=" mb-2">Devido a que</h3>
             <li> T' → * F T' | ε</li>
-            <li>Dada esta produccion se tiene que el primer elemento de la produccion 
-              de T' es <strong className=" text-xl">*</strong> por lo cual se coloca en el P(T') el <strong className=" text-xl">*</strong> </li>
             <li>
-            Tambien se tiene que en esta produccion el primer elemento 
-            puede ser <strong className=" text-lg">ε</strong> por lo cual se coloca en el P(T') el 
+              Dada esta produccion se tiene que el primer elemento de la
+              produccion de T' es <strong className=" text-xl">*</strong> por lo
+              cual se coloca en el P(T') el{" "}
+              <strong className=" text-xl">*</strong>{" "}
+            </li>
+            <li>
+              Tambien se tiene que en esta produccion el primer elemento puede
+              ser <strong className=" text-lg">ε</strong> por lo cual se coloca
+              en el P(T') el
               <strong className=" text-lg"> ε</strong>
             </li>
           </ul>
-          <h3 className="text-lg font-semibold mt-4 mb-2">
-            Cálculo del Conjunto Siguiente:{/* Siguiente */}
+          <h3 className="text-2xl font-extrabold mt-4 mb-2 font-serif bg-green-300 ">
+            Cálculo del Conjunto Siguiente:
+            {/* Calculo del conjunto Siguiente */}
           </h3>
           <pre className="bg-white p-2 mt-4 rounded-md whitespace-pre-wrap font-bold">
             {/* perte de la E */}
@@ -385,21 +424,20 @@ S(T) = { +, ), $ }`}
             <li>E → T E' </li>
             <li>
               Dada esta producción se tiene que el elemento siguiente de T es{" "}
-              <strong className=" text-lg">E'</strong>por lo cual siguiendo
-              las reglas del conjunto siguiente se colocara el{" "}
+              <strong className=" text-lg">E'</strong>por lo cual siguiendo las
+              reglas del conjunto siguiente se colocara el{" "}
               <strong className=" text-lg">P(E')</strong> en el S(T)
             </li>
             <li>E' → + T E' | ε</li>
             <li>
               Dada esta producción se tiene que el elemento siguiente de T es{" "}
               <strong className=" text-lg">E'</strong> pero este ya esta
-              agregado 
-              en la anterior producción
+              agregado en la anterior producción
             </li>
             <li>
               Continuando con la producción anterior se tiene que E' puede ser
-              vacio, por lo cual se verian 
-              asi las producciónes tomando en cuenta lo anterior
+              vacio, por lo cual se verian asi las producciónes tomando en
+              cuenta lo anterior
             </li>
             <li>E → T </li>
             <li>
@@ -423,8 +461,8 @@ S(T') = { +, ), $ }`}
             <li>T → F T'</li>
             <li>
               Dada esta producción se tiene que no hay un elemento siguiente
-              despues de T' por lo cual al seguir 
-              las reglas del conjunto siguiente se colocara el{" "}
+              despues de T' por lo cual al seguir las reglas del conjunto
+              siguiente se colocara el{" "}
               <strong className=" text-lg">S(T)</strong>{" "}
             </li>
           </ul>
@@ -450,7 +488,8 @@ S(F) = { *, +, ), $ }
             </li>
             <li>
               Continuando con la producción anterior se tiene que T' puede ser
-              vacio, por lo cual se verian asi las producciónes tomando en cuenta lo anterior
+              vacio, por lo cual se verian asi las producciónes tomando en
+              cuenta lo anterior
             </li>
             <li>T' → * F </li>
             <li>
@@ -465,7 +504,7 @@ S(F) = { *, +, ), $ }
               el S(F)
             </li>
           </ul>
-        </div>
+        </div>{/* Aqui deberia de terminar la seccion de los conjuntos */}
       </div>
     </div>
   );
